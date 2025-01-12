0c2eac14a9d2630738332e0919325d2c610d54be
xiewen <1123654054@qq.com>
2025-01-12 11:06:48 +0800

    环境变量配置

--- mall-web/src/main.ts
+++ mall-web/src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+console.log(111, import.meta.env.VITE_age);
+createApp(App).mount("#app");
--- mall-web/src/vite-env.d.ts
+++ mall-web/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// 接口合并，env环境变量的类型
+interface ImportMetaEnv {
+  VITE_username: string;
+  VITE_age: string;
+}
