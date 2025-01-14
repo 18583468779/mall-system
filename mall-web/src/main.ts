c85a3e798d5b0208482d5762d6e2e967a8fd33ef
xiewen <1123654054@qq.com>
2025-01-14 19:46:10 +0800

    开发环境的配置

--- mall-web/src/main.ts
+++ mall-web/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-console.log(111, import.meta.env.VITE_age);
 createApp(App).mount("#app");
--- mall-web/vite.config.ts
+++ mall-web/vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from "vite";
+import { CommonServerOptions, ConfigEnv, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "dang/", // 基础路劲
-  plugins: [vue()],
+export default defineConfig((mode: ConfigEnv) => {
+  const envName: string = ".env";
+  const curEnvFileName = `${envName}.${mode.mode}`;
+  let server: CommonServerOptions = {};
+  if (mode.mode === "development") {
+    console.log("开发环境", curEnvFileName);
+    server = {
+      // 配置代理
+      port: 5005,
+      proxy: {
+        "/dang": {
+          target: "http:localhost:5003/",
+        },
+      },
+    };
+  } else if (mode.mode === "production") {
+    console.log("生产环境", curEnvFileName);
+  }
+  return {
+    plugins: [vue()],
+    server,
+  };
 });
