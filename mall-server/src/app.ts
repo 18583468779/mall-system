3296b48fe41e5cf823d0714bb6788f2121e432e2
xiewen <1123654054@qq.com>
2025-01-17 19:00:52 +0800

    数据库封装

--- mall-server/src/app.ts
+++ mall-server/src/app.ts
@@ -1,4 +1,9 @@
 import Koa from "koa";
 import allRouterLoader from "./common/AllRouterLoader";
+import dbconfig from "./config/dbconfig";
 const app = new Koa();
+
+console.log(dbconfig.getConf("database"));
+console.log(dbconfig.getConf());
+
 allRouterLoader.init(app);
