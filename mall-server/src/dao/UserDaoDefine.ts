f91ccabf15fff14153bc72fd608efd593e52151f
xiewen <1123654054@qq.com>
2025-01-18 20:36:58 +0800

    模糊查询

--- mall-server/src/dao/UserDaoDefine.ts
+++ mall-server/src/dao/UserDaoDefine.ts
@@ -1,11 +1,48 @@
+import { Op } from "sequelize";
 import { model } from "../definemodel";
 
 class UserDaoDefine {
   static addUser(userinfo: Userinfo) {
-    return model.create(userinfo);
+    return model.create(userinfo); // 新增一个用户
+  }
+  static findAllUser() {
+    return model.findAll({
+      raw: true,
+    }); // 查询所有用户
+  }
+  static findByProps() {
+    return model.findAll({
+      raw: true,
+      attributes: ["username", "password"], // 只查询用户名和密码
+    });
+  }
+  static findByUsmAndPsw(username: string, password: string) {
+    return model.findOne({
+      //查询用户名和密码
+      raw: true,
+      where: {
+        [Op.and]: [{ username }, { password }],
+      },
+    });
+  }
+  static findByLike() {
+    return model.findAll({
+      raw: true,
+      where: {
+        username: {
+          [Op.like]: "王%", //模糊查询
+        },
+      },
+    });
   }
 }
-export const { addUser } = UserDaoDefine;
+export const {
+  addUser,
+  findAllUser,
+  findByProps,
+  findByUsmAndPsw,
+  findByLike,
+} = UserDaoDefine;
 export type Userinfo = {
   userid: number;
   username: string;
--- mall-server/src/router/user.ts
+++ mall-server/src/router/user.ts
@@ -1,5 +1,12 @@
 import { success } from "../common/ResResult";
-import { addUser, Userinfo } from "../dao/UserDaoDefine";
+import {
+  addUser,
+  findAllUser,
+  findByLike,
+  findByProps,
+  findByUsmAndPsw,
+  Userinfo,
+} from "../dao/UserDaoDefine";
 import { Context } from "koa";
 import Router from "koa-router";
 
@@ -15,5 +22,29 @@ router.post("/addUser", async (ctx: Context) => {
   const userinfo: Userinfo = ctx.request.body;
   const dbUserinfo = await addUser(userinfo);
   ctx.body = success(dbUserinfo);
+});
+
+router.get("/findAllUser", async (ctx: Context) => {
+  // 查询所有用户
+  const dbAllUser = await findAllUser();
+  ctx.body = success(dbAllUser);
+});
+
+router.get("/findByProps", async (ctx: Context) => {
+  //只查询用户名和密码字段
+  const dbAllUser = await findByProps();
+  ctx.body = success(dbAllUser);
+});
+
+router.post("/findByUsmAndPsw", async (ctx: Context) => {
+  const { username, password } = ctx.request.body;
+  const dbUserOne = await findByUsmAndPsw(username, password);
+  ctx.body = success(dbUserOne);
+});
+
+router.get("/findByLike", async (ctx: Context) => {
+  // 模糊查询
+  const dbAllUser = await findByLike();
+  ctx.body = success(dbAllUser);
 });
 module.exports = router;
