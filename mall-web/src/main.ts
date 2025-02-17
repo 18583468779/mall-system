033429d2585aa1994852ea8831bc9bcae4f8f6db
xiewen <1123654054@qq.com>
2025-02-17 21:48:25 +0800

    axios封装完成

--- mall-web/src/main.ts
+++ mall-web/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/index.css";
 import App from "./App.vue";
-createApp(App).mount("#app");
+createApp(App).use(ElementPlus, { size: "small" }).mount("#app");
--- mall-web/src/utils/axiosUtil.ts
+++ mall-web/src/utils/axiosUtil.ts
@@ -1,11 +1,24 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from "axios";
 import { ElMessage } from "element-plus";
-
+import conf from "../config";
 const SERVER_ERR = "请求服务器的网址错误或网络连接失败";
 class AxiosUtil {
   static axiosUtil: AxiosUtil = new AxiosUtil();
   axiosInstance!: AxiosInstance;
-  constructor() {}
+  request!: ReqExecute;
+  constructor() {
+    this.request = {
+      get: (): any => {},
+      post: (): any => {},
+      put: (): any => {},
+      delete: (): any => {},
+      patch: (): any => {},
+    };
+    this.createAxiosInstance();
+    this.beforeReqIntercpt();
+    this.beforeResponseIntercpt();
+    this.reqPrepare();
+  }
   createAxiosInstance() {
     this.axiosInstance = axios.create({ timeout: 15000 });
   }
@@ -35,8 +48,41 @@ class AxiosUtil {
     );
   }
   // 3.发送请求给服务器
-  sendRequest(options: any) {}
+  sendRequest(options: AxiosRequestConfig_) {
+    if (conf.env === "production") {
+      this.axiosInstance.defaults.baseURL = conf.baseApi;
+    } else if (conf.env === "development") {
+      const isMock: boolean = options.isMock || conf.isMock;
+      this.axiosInstance.defaults.baseURL = isMock
+        ? conf.mockBaseApi
+        : conf.baseApi;
+    }
+    return this.axiosInstance(options); // 返回一个 AxiosPromise<any> 对象
+  }
 
   // 4. 深入灵活应用ts完成请求method类型自动提示
-  reqPrepare() {}
+  reqPrepare() {
+    return methods.forEach((method) => {
+      this.request[method] = (url, isMock, data) => {
+        return this.sendRequest({ url, isMock, data });
+      };
+    });
+  }
 }
+interface AxiosRequestConfig_ extends AxiosRequestConfig {
+  isMock: boolean;
+}
+
+type Method = "get" | "post" | "put" | "delete" | "patch";
+
+const methods: Method[] = ["get", "post", "put", "delete", "patch"];
+type ReqFn = (url: string, isMock: boolean, data: any) => AxiosPromise<any>;
+interface ReqExecute {
+  get: ReqFn;
+  post: ReqFn;
+  put: ReqFn;
+  delete: ReqFn;
+  patch: ReqFn;
+}
+
+export default AxiosUtil.axiosUtil.request;
