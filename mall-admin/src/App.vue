a81903b0910fd8eccf715e8bde9650f43f9c6a6c
xiewen <1123654054@qq.com>
2025-04-08 19:40:21 +0800

    vue admin重构

--- mall-admin/src/App.vue
+++ mall-admin/src/App.vue
@@ -1,59 +1,28 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useAppStore } from '@/store/modules/app'
-import { ConfigGlobal } from '@/components/ConfigGlobal'
-import { useDesign } from '@/hooks/web/useDesign'
-import { ElNotification } from 'element-plus'
-
-const { getPrefixCls } = useDesign()
-
-const prefixCls = getPrefixCls('app')
-
-const appStore = useAppStore()
-
-const currentSize = computed(() => appStore.getCurrentSize)
-
-const greyMode = computed(() => appStore.getGreyMode)
-
-appStore.initTheme()
-
-ElNotification({
-  title: '提示',
-  type: 'warning',
-  duration: 0,
-  dangerouslyUseHTMLString: true,
-  message:
-    '<div><p><strong>遇事不决，请先查阅常见问题，说不定你能找到相关解答</strong></p><p><a href="https://element-plus-admin-doc.cn/guide/fqa.html" target="_blank">链接地址</a></p></div>'
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <ConfigGlobal :size="currentSize">
-    <RouterView :class="greyMode ? `${prefixCls}-grey-mode` : ''" />
-  </ConfigGlobal>
+  <div class="">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div class="bg-red-700">111</div>
 </template>
 
-<style lang="less">
-@prefix-cls: ~'@{adminNamespace}-app';
-
-.size {
-  width: 100%;
-  height: 100%;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-html,
-body {
-  padding: 0 !important;
-  margin: 0;
-  overflow: hidden;
-  .size;
-
-  #app {
-    .size;
-  }
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-.@{prefix-cls}-grey-mode {
-  filter: grayscale(100%);
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ mall-admin/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // 包含 Vue 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
