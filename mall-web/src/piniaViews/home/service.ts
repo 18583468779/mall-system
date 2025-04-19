43353f6bdb997d75b0f4788c5e36ef8321561563
xiewen <1123654054@qq.com>
2025-04-19 20:30:19 +0800

    给图书设置添加购物车数量

--- mall-web/src/piniaViews/home/service.ts
+++ mall-web/src/piniaViews/home/service.ts
@@ -1,10 +1,28 @@
 import { storeToRefs } from "pinia";
 import bookStore from "../../piniaStore/book";
+import shopListService from "../books/service/shopCart";
+
 export default class HomeClass {
   static homeClass: HomeClass = new HomeClass();
   static store = bookStore();
   static storeRef = storeToRefs(HomeClass.store);
   static async getBookListByPage(page = 1, pageSize = 8) {
     await HomeClass.store.getBookListByPage(page, pageSize);
+    addCartNumToBookList(
+      HomeClass.store.getAllBookList,
+      shopListService.store.getShopCartList
+    );
   }
+}
+
+// 将商品购物车数量添加到图书列表中
+function addCartNumToBookList(bookList: any[], cartList: any[]) {
+  bookList.map((book) => {
+    const cartItem = cartList.find((item) => item.bookisbn === book.ISBN);
+    if (cartItem) {
+      book.purcharsenum = cartItem.purcharsenum; // 将购物车数量添加到图书列表中
+    } else {
+      book.purcharsenum = 0; // 将购物车数量添加到图书列表中
+    }
+  });
 }
